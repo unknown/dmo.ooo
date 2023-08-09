b33c6f33fbea8a8f9081a1deec2981dd3094f983
David Mo <44306450+unknown@users.noreply.github.com>
2023-08-09 11:33:28 -0400

    Add hover effect to segmented control

--- src/components/SegmentedControl.tsx
+++ src/components/SegmentedControl.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import React, { MouseEvent } from "react";
 import { twMerge } from "tailwind-merge";
 
 type SegmentedControlProps = {
@@ -8,12 +9,36 @@ type SegmentedControlProps = {
 };
 
 export function SegmentedControl({ options, selected, onSelect }: SegmentedControlProps) {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+    const { left, top } = currentTarget.getBoundingClientRect();
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
   return (
-    <div className="flex rounded-full border border-gray-200 bg-gray-100/80 p-1 shadow-inner backdrop-blur-sm">
+    <div
+      className="relative group flex rounded-full border border-gray-200 bg-gray-100/80 p-1 shadow-inner backdrop-blur-sm"
+      onMouseMove={handleMouseMove}
+    >
+      <motion.div
+        className="-z-10 pointer-events-none absolute inset-0 rounded-full opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              250px circle at ${mouseX}px ${mouseY}px,
+              rgba(255, 255, 255, 0.65),
+              transparent 80%
+            )
+          `,
+        }}
+      />
       {options.map((item, i) => (
         <button
           key={item.key}
-          className="relative rounded-full px-4 py-2 hover:text-gray-600 transition-colors"
+          className="relative rounded-full px-4 py-2"
           onClick={() => onSelect(i)}
         >
           <div
