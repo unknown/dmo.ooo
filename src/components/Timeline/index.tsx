0412dff34efed3ad1d4dc8c68a6721929720a92b
David Mo <david@dmo.ooo>
2025-05-02 13:40:47 -0400

    feat: spring timeline

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -8,7 +8,7 @@ export const Timeline: TimelineComponent = ({ posts }) => {
       {posts.map((post, i) => (
         <motion.div
           key={post.title}
-          transition={{ duration: 0.2 }}
+          transition={{ duration: 0.5, type: "spring" }}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
